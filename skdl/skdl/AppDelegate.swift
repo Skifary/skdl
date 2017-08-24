711723017a7fe7dfd82f6549b1e033e04a823146
Skifary <376512563@qq.com>
2017-08-24 11:32:04 +0800

    create skdl

---
+++ skdl/skdl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  skdl
+//
+//  Created by Skifary on 24/08/2017.
+//  Copyright © 2017 skifary. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ skdl/skdl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  skdl
+//
+//  Created by Skifary on 24/08/2017.
+//  Copyright © 2017 skifary. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
