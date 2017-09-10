652110115fdae2c2f833f95e6240841a3ea7db35
Skifary <376512563@qq.com>
2017-09-10 14:27:21 +0800

    add bridging header

---
+++ skdl/skdl/skdl-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  skdl-Bridging-Header.h
+//  skdl
+//
+//  Created by Skifary on 10/09/2017.
+//  Copyright © 2017 skifary. All rights reserved.
+//
+
+#ifndef skdl_Bridging_Header_h
+#define skdl_Bridging_Header_h
+
+#import "MASPreferences.h"
+
+#endif /* skdl_Bridging_Header_h */
